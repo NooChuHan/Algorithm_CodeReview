1cf649b04f6f8b4c17275903de0dca3260a8f096
kimseungdeok <tmdejr1117@gmail.com>
2023-06-26 20:25:01 +0900

    fix : 가독성을 위한 코드 이동

--- week3/김승덕/디스크 컨트롤러.js
+++ week3/김승덕/디스크 컨트롤러.js
@@ -1,35 +1,3 @@
-function solution(jobs) {
-    const count = jobs.length; // 작업 개수를 세는 변수
-    const minHeap = new MinHeap(); // 최소 힙 객체 생성
-    jobs.sort((a, b) => a[0] - b[0]); // 작업을 요청 시간 기준으로 정렬
-
-    let time = 0; // 시간 변수 초기화
-    let complete = 0; // 완료된 작업 개수 초기화
-    let total = 0; // 작업의 총 소요 시간 초기화
-
-    while (jobs.length || minHeap.size()) {
-        // 작업 배열이 비어있지 않거나 최소 힙이 비어있지 않은 동안 반복
-        while (jobs.length) {
-            if (jobs[0][0] === time) {
-                // 작업의 요청 시간이 현재 시간과 일치하는 경우
-                minHeap.heappush(jobs.shift()); // 해당 작업을 최소 힙에 추가
-            } else {
-                break;
-            }
-        }
-
-        if (minHeap.size() && time >= complete) {
-            // 최소 힙이 비어있지 않고, 현재 시간이 완료된 작업 시간 이후인 경우
-            const task = minHeap.heappop(); // 최소 힙에서 가장 우선 순위가 높은 작업 추출
-            complete = task[1] + time; // 작업의 완료 시간 계산
-            total += complete - task[0]; // 작업의 총 소요 시간에 추가
-        }
-        time++; // 시간 증가
-    }
-
-    return (total / count) >> 0; // 작업의 평균 소요 시간 반환 (정수형으로 변환)
-}
-
 class MinHeap {
     constructor() {
         this.heap = [null]; // 힙 배열 생성 및 첫 번째 요소는 null로 초기화
@@ -101,4 +69,36 @@ class MinHeap {
 
         return min; // 최소값 반환
     }
+}
+
+function solution(jobs) {
+    const count = jobs.length; // 작업 개수를 세는 변수
+    const minHeap = new MinHeap(); // 최소 힙 객체 생성
+    jobs.sort((a, b) => a[0] - b[0]); // 작업을 요청 시간 기준으로 정렬
+
+    let time = 0; // 시간 변수 초기화
+    let complete = 0; // 완료된 작업 개수 초기화
+    let total = 0; // 작업의 총 소요 시간 초기화
+
+    while (jobs.length || minHeap.size()) {
+        // 작업 배열이 비어있지 않거나 최소 힙이 비어있지 않은 동안 반복
+        while (jobs.length) {
+            if (jobs[0][0] === time) {
+                // 작업의 요청 시간이 현재 시간과 일치하는 경우
+                minHeap.heappush(jobs.shift()); // 해당 작업을 최소 힙에 추가
+            } else {
+                break;
+            }
+        }
+
+        if (minHeap.size() && time >= complete) {
+            // 최소 힙이 비어있지 않고, 현재 시간이 완료된 작업 시간 이후인 경우
+            const task = minHeap.heappop(); // 최소 힙에서 가장 우선 순위가 높은 작업 추출
+            complete = task[1] + time; // 작업의 완료 시간 계산
+            total += complete - task[0]; // 작업의 총 소요 시간에 추가
+        }
+        time++; // 시간 증가
+    }
+
+    return (total / count) >> 0; // 작업의 평균 소요 시간 반환 (정수형으로 변환)
 }
