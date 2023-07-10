507d9ae11dc0d7bc4e9f5768d0c93d64460300e2
kimseungdeok <tmdejr1117@gmail.com>
2023-07-10 20:14:42 +0900

    chore : 주석 추가

--- week5/김승덕/전력망을 둘로 나누기.js
+++ week5/김승덕/전력망을 둘로 나누기.js
@@ -8,44 +8,56 @@
 
 function solution(n, wires) {
     var answer = Number.MAX_SAFE_INTEGER;
-    
-    let tree = Array.from(Array(n + 1), () => []);
 
     // 트리 생성
-    wires.map((element) => {
-        let [a, b] = element;
+    let tree = Array.from(Array(n + 1), () => []); // 트리 배열 생성
 
-        tree[a].push(b);
-        tree[b].push(a);
+    wires.map((element) => {
+        // wires 배열을 순회하며
+        let [a, b] = element; // 요소를 [a, b]로 분해하여 변수 a와 b에 할당
+
+        tree[a].push(b); // a와 연결된 노드 b를 tree[a]에 추가
+        tree[b].push(a); // b와 연결된 노드 a를 tree[b]에 추가
     });
-    // bfs 
+
+    // bfs로 트리 탐색하여 노드 간 거리(간선 수) 계산하는 함수
     function searchTree(root, exceptNum) {
-        let count = 0;
-        let visit = [];
-        let queue = [root];
-        visit[root] = true;
+        let count = 0; // 거리(간선 수)를 세는 변수
+        let visit = []; // 방문 여부를 표시하는 배열
+        let queue = [root]; // 탐색을 위한 큐에 root 노드를 추가
+        visit[root] = true; // root 노드를 방문했음을 표시
+
         while (queue.length) {
-            let index = queue.pop();
+            // 큐가 비어있지 않은 동안 반복
+            let index = queue.pop(); // 큐에서 요소를 하나 가져옵니다.
+
             tree[index].forEach((element) => {
+                // 해당 노드와 연결된 노드들에 대해 반복
                 if (element !== exceptNum && visit[element] !== true) {
-                    visit[element] = true;
-                    queue.push(element);
+                    // exceptNum과 같지 않고, 아직 방문하지 않은 경우
+                    visit[element] = true; // 방문했음을 표시
+                    queue.push(element); // 큐에 노드를 추가
                 }
             });
-            count++;
+            count++; // 거리(간선 수)를 증가시킵니다.
         }
 
-        return count;
+        return count; // 거리(간선 수)를 반환
     }
-    // 트리를 순회하며 최소값을 찾는다.
+
+    // 트리를 순회하며 최소값을 찾습니다.
     wires.forEach((element) => {
-        let [a, b] = element;
+        // wires 배열을 순회하며
+        let [a, b] = element; // 요소를 [a, b]로 분해하여 변수 a와 b에 할당
+
+        // searchTree(a, b)와 searchTree(b, a)를 계산한 절댓값을 비교하여 최솟값을 answer에 할당
         answer = Math.min(
             answer,
             Math.abs(searchTree(a, b) - searchTree(b, a))
         );
     });
-    return answer;
+
+    return answer; // 최솟값을 반환
 }
 
 solution(9, [
